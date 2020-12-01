784ec322b3591ab01c416506245f3174dd83f1e0
Babatunde Adegbite <babatunde.adeyemi@izea.com>
2020-12-01 01:09:25 +0100

    making changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,15 @@
 	var myApp = angular.module('myApp', ['ui.router', 'myApp.controllers', 'myApp.models']);
 
 	// configure our routes
-	myApp.config(function($stateProvider, $urlRouterProvider) {
-	  //
+	myApp.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+		//
+		$locationProvider.html5Mode(true);
 	  // For any unmatched url, redirect to /state1
-	  $urlRouterProvider.otherwise("/");
+		$urlRouterProvider.otherwise(function($injector) {
+			$injector.invoke(function($state) {
+					$state.transitionTo("404", {}, false);
+			});
+		});
 	  //
 	  // Now set up the states
 	  $stateProvider
@@ -39,5 +44,9 @@
 			url: "/contact",
 			templateUrl: "partials/contact.html"
 		})
+
+		.state('404', {
+			templateUrl: 'partials/404.html'
+	 })
 
 	});
